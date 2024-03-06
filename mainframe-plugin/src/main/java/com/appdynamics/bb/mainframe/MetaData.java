89600ed6f6dbd33f5754e762ee20a0cc2bfc5311
Fernando Dumont <fernando@fernando.com.br>
2024-03-06 18:47:05 -0300

    Fix values pom.xml

--- mainframe-plugin/src/main/java/com/appdynamics/bb/mainframe/MetaData.java
+++ mainframe-plugin/src/main/java/com/appdynamics/bb/mainframe/MetaData.java
@@ -1,6 +1,6 @@
 package com.appdynamics.bb.mainframe;
 
 public class MetaData {
-    public static final String VERSION = "v1.1";
+    public static final String VERSION = "v1.0";
     public static final String BUILDTIMESTAMP = "20240306";
 }
